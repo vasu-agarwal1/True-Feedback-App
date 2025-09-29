bb25bd3b3af4bd5ccb38c8bcf7e82638e35a0c44
Vasu Agarwal <agarwal.vasu32@gmail.com>
2025-09-30 00:25:14 +0530

    Add initial project setup with components, styles, and dependencies
    - Created components.json for component configuration
    - Added global TypeScript declarations in global.d.ts
    - Updated package-lock.json and package.json with new dependencies
    - Implemented chai page with a button component
    - Enhanced global styles in globals.css
    - Refactored Home page to include a greeting
    - Introduced reusable Button component with variants
    - Added utility function for class name merging

---
+++ src/app/chai/page.tsx
@@ -0,0 +1,15 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+const chaiPage = () => {
+  return (
+    <main className="h-full flex justify-center items-center ">
+    <div>
+      <h1>Chai Page</h1>
+    </div>
+    <Button>Button</Button>
+    </main>
+  )
+}
+
+export default chaiPage
