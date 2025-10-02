b0c8f37ab3ed0f374c90935d5a94bbfb5626642c
Vasu Agarwal <agarwal.vasu32@gmail.com>
2025-10-02 23:48:23 +0530

    feat: Implement code verification API with user feedback

---
+++ src/app/api/verify-code/route.ts
@@ -0,0 +1,70 @@
+import dbConnect from "@/lib/dbConnect";
+import userModel from "@/model/User";
+
+
+export async function POST(request: Request) {
+    await dbConnect()
+
+    try {
+        const { username, code } = await request.json();
+
+        const decodedUsername = decodeURIComponent(username);
+        const user = await userModel.findOne({ username: decodedUsername });
+
+        if(!user) {
+            return Response.json(
+                {
+                    success: false,
+                    message: "User not found",
+                },
+                { status: 404 }
+            );
+        }
+
+        const isCodeValid = user.verifyCode === code
+        const isCodeNotExpired = new Date(user.verifyCodeExpiry) > new Date()
+        if (isCodeValid && isCodeNotExpired) {
+            user.isVerified = true;
+            await user.save()
+
+            return Response.json(
+                {
+                    success: true,
+                    message: "Code verified successfully",
+                },
+                { status: 200 }
+            );
+
+        }else if(!isCodeNotExpired){
+            return Response.json(
+                {
+                    success: false,
+                    message: "Code has expired",
+                },
+                { status: 400 }
+            );
+        } else {
+            return Response.json(
+                {
+                    success: false,
+                    message: "Invalid code",
+                },
+                { status: 400 }
+            );
+        }
+        
+    } catch (error) {
+        console.error("Error verifying code:", error);
+
+        return Response.json(
+            {
+                success: false,
+                message: "Error verifying code",
+            },
+            { status: 500 }
+        );
+    
+     }
+        
+}
+
