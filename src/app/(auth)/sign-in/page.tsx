dd567ca6d086aa7b15ca8111801e79037af86928
Vasu Agarwal <agarwal.vasu32@gmail.com>
2025-10-05 23:28:07 +0530

    feat: Create dashboard and layout components; implement sign-in page with form handling and validation

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,20 +1,105 @@
 'use client'
-import { useSession, signIn, signOut } from "next-auth/react"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import * as z  from "zod"
+import Link from "next/link"
+import { useEffect, useState } from "react"
+import { useDebounceCallback, useDebounceValue } from 'usehooks-ts'
+import { toast } from "sonner"
+import { useRouter } from "next/navigation"
+import { signUpSchema } from "@/schemas/signUpSchema"
+import axios, { AxiosError } from 'axios'
+import { ApiResponse } from "@/types/ApiResponse"
+import { set } from "mongoose"
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
+import { Loader2 } from "lucide-react"
+import { signInSchema } from "@/schemas/signInSchema"
+import { signIn } from "next-auth/react"
 
-export default function Component() {
-  const { data: session } = useSession()
-  if (session) {
-    return (
-      <>
-        Signed in as {session.user.email} <br />
-        <button onClick={() => signOut()}>Sign out</button>
-      </>
-    )
-  }
+const page = () => {
+
+    const router = useRouter()
+
+    //zod implementation
+    const form = useForm({
+        resolver: zodResolver(signInSchema),
+        defaultValues: {
+            identifier: '',
+            password: ''
+        }
+    })
+
+
+    const onSubmit = async (data: z.infer<typeof signInSchema>) => {
+       const result = await signIn('credentials', {
+        redirect: false,
+        identifier: data.identifier,
+        password: data.password
+       })
+       if(result?.error){
+        toast("SignIn failed", {description: "Incorrect username or password"})
+       }
+       if(result?.url){
+        router.replace('/dashboard')
+       }
+    }
   return (
-    <>
-      Not signed in <br />
-      <button className="bg-amber-700 p-3 m-4 rounded" onClick={() => signIn()}>Sign in</button>
-    </>
+    <div className="flex justify-center items-center min-h-screen bg-gray-100">
+        <div className="w-full max-w-md p-8 space-y-8 bg-white rounded-lg shadow-md">
+            <div className="text-center">
+              <h1 className=" text-4xl font-extrabold tracking-tight lg:text-5xl mb-6">
+                Join Mystery Message
+               </h1>
+              <p className="mb-4" >Sign In to start your anonymous journey</p>
+            </div>
+            <Form {...form}>
+                <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+            <FormField
+                    name="identifier"
+                    control={form.control}
+                    render={({ field }) => (
+                        <FormItem>
+                          <FormLabel>Email/Username</FormLabel>
+                          <FormControl>
+                            <Input placeholder="email/username" {...field}
+                             />
+                          </FormControl>
+                         <FormMessage />
+                        </FormItem>
+              )}
+         />
+            <FormField
+                    name="password"
+                    control={form.control}
+                    render={({ field }) => (
+                        <FormItem>
+                          <FormLabel>Password</FormLabel>
+                          <FormControl>
+                            <Input type="password" placeholder="password" {...field}
+                             />
+                          </FormControl>
+                         <FormMessage />
+                        </FormItem>
+              )}
+         />
+         <Button type="submit">
+            Sign In
+         </Button>
+        </form>
+        </Form>
+        <div className="text-center mt-4">
+            <p>
+                Already a member?{' '}
+                <Link href="/sign-in" className="text-blue-500 hover:text-blue-800">
+                    Sign In
+                </Link>
+            </p>
+            </div>
+        </div>
+    </div>
   )
 }
+
+export default page
