f75c4d0b9de7d43310250edecff91a75ffc69cc2
Vasu Agarwal <agarwal.vasu32@gmail.com>
2025-10-06 16:39:37 +0530

    feat: implement MessageCard component and enhance Navbar with user greeting

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,15 +3,30 @@ import React from 'react'
 import Link from 'next/link'
 import { signOut, useSession } from 'next-auth/react'
 import { User } from "next-auth";
+import { Button } from './ui/button';
 const Navbar = () => {
   const { data: session } = useSession();
   
   const user = session?.user as User
 
   return (
-    <div>
-      
-    </div>
+    <nav className='p-4 md:p-6 shadow-md'>
+      <div className='container mx-auto flex flex-col md:flex-row justify-between items-center '>
+        <a className='text-xl font-bold mb-4 mb:mb-0' href="#">Mystery Message</a>
+        {
+          session ? (
+            <>
+            <span className='mr-4'>Welcome, {user?.username || user?.email}</span>
+            <Button className='w-full md:w-auto' onClick={() => signOut()}>Logout</Button>
+            </>
+          ) : (
+            <Link href={'/sign-in'}>
+              <Button className='w-full md:w-auto'>Login</Button>
+            </Link>
+          )
+        }
+      </div>
+    </nav>
   )
 }
 
