ef1b47fe25719247565d85c8549596c665770dd8
Vasu Agarwal <agarwal.vasu32@gmail.com>
2025-10-08 13:39:24 +0530

    style: update background color for sign-in and verify account pages for consistency

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -39,8 +39,8 @@ const page = () => {
        }
     }
   return (
-    <div className="flex justify-center items-center min-h-screen bg-gray-100">
-        <div className="w-full max-w-md p-8 space-y-8 bg-white rounded-lg shadow-md">
+    <div className="flex justify-center items-center min-h-screen bg-gray-800">
+        <div className="w-full max-w-md p-7 space-y-8 bg-white rounded-lg shadow-md">
             <div className="text-center">
               <h1 className=" text-4xl font-extrabold tracking-tight lg:text-5xl mb-6">
                 Join Mystery Message
@@ -77,7 +77,7 @@ const page = () => {
                         </FormItem>
               )}
          />
-         <Button type="submit">
+         <Button className="w-full" type="submit">
             Sign In
          </Button>
         </form>
