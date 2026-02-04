bd3317474a06af6fbc1b84da408adf032414d02e
Vasu Agarwal <agarwal.vasu32@gmail.com>
2026-02-04 17:36:59 +0530

    fix: remove console log from username validation in route handler

--- next.config.ts
+++ next.config.ts
@@ -7,10 +7,9 @@ const nextConfig: NextConfig = {
     ignoreDuringBuilds: true,
   },
   typescript: {
-    // !! WARN !!
     // Dangerously allow production builds to successfully complete even if
     // your project has type errors.
-    // !! WARN !!
+    
     ignoreBuildErrors: true,
   },
 };
--- src/app/api/check-username-unique/route.ts
+++ src/app/api/check-username-unique/route.ts
@@ -19,7 +19,6 @@ export async function GET(request: Request){
         //validate with zod
         const result = UsernamQuerySchema.safeParse(queryParam)
 
-        console.log(result) //TODO: remove this
 
         if(!result.success) {
             const usernameErrors = result.error.format().
